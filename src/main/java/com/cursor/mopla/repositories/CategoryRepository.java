c31fb7927d18cd90466ee329087904553427d047
KrasuliaV <Fenix2104>
2021-06-19 11:53:08 +0300

    Added CategoryRepository Updated MovieRepository

--- src/main/java/com/cursor/mopla/repositories/CategoryRepository.java
+++ src/main/java/com/cursor/mopla/repositories/CategoryRepository.java
@@ -1,9 +1,9 @@
 package com.cursor.mopla.repositories;
 
-import com.cursor.mopla.entities.Movie;
+import com.cursor.mopla.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MovieRepository extends JpaRepository<Movie, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
