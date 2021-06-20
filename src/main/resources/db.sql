8c0510eb0a64aab2069e203c57edca0c75d0781b
KrasuliaV <Fenix2104>
2021-06-20 22:52:32 +0300

    Updated resources

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,4 +1,4 @@
-insert into category
+insert into category (id, short_name, full_name)
 values (1, 'WEST', 'Western'),
        (2, 'ADVENT', 'Adventure'),
        (3, 'HOR', 'Horror'),
@@ -12,38 +12,36 @@ values (1, 'WEST', 'Western'),
        (11, 'SP', 'Sport'),
        (12, 'SUP', 'Superhero');
 
-insert into users
-values (1, 'admin', 'admin', 'ROLE_ADMIN'),
-       (2, 'user', 'user', 'ROLE_USER'),
-       (3, 'kesha', '12345', 'ROLE_USER'),
-       (4, 'niavchik', '12345', 'ROLE_USER'),
-       (5, 'zhorzhetta', '12345', 'ROLE_USER'),
-       (6, 'marietta', '12345', 'ROLE_USER'),
-       (7, 'kolietta', 'admin', 'ROLE_USER');
+insert into users (id, username, password, role)
+values (1, 'admin', '$2y$08$BQ8qMDigdyJYbdmduW7A7efV9YRtlF9IFWo2gVYUZZLDIkk.magaC', 'ROLE_ADMIN'),
+       (2, 'user', '$2y$08$aHL/sP3mXnWKWii90Nb7cOCFpg3K2DRyc5O0E9b1vyDanli99ZQAy', 'ROLE_USER'),
+       (3, 'kesha', '$2y$08$Q.oOWJ92JLe/e5CqW8ERduBpdfieKltqKoL3U4I0fkdFck9ViaRC2', 'ROLE_USER'),
+       (4, 'niavchik', '$2y$08$Q.oOWJ92JLe/e5CqW8ERduBpdfieKltqKoL3U4I0fkdFck9ViaRC2', 'ROLE_USER'),
+       (5, 'zhorzhetta', '$2y$08$Q.oOWJ92JLe/e5CqW8ERduBpdfieKltqKoL3U4I0fkdFck9ViaRC2', 'ROLE_USER'),
+       (6, 'marietta', '$2y$08$Q.oOWJ92JLe/e5CqW8ERduBpdfieKltqKoL3U4I0fkdFck9ViaRC2', 'ROLE_USER'),
+       (7, 'kolietta', '$2y$08$Q.oOWJ92JLe/e5CqW8ERduBpdfieKltqKoL3U4I0fkdFck9ViaRC2', 'ROLE_USER');
 
-insert into movie
-values (1, 'Rocky', 'John G. Avildsen', 'It tells the rags to riches American Dream story of Rocky Balboa'),
---                          sport, drama
+insert into movie (id, name, director, description, votes_count, value)
+values (1, 'Rocky', 'John G. Avildsen', 'It tells the rags to riches American Dream story of Rocky Balboa', 2, 1.00),
        (2, 'Seven Pounds', 'Gabriele Muccino',
-        'A man with a fateful secret embarks on an extraordinary journey of redemption by forever changing the lives of seven strangers'),
---                          drama
+        'A man with a fateful secret embarks on an extraordinary journey of redemption by forever changing the lives of seven strangers',
+        3, 10.00),
        (3, 'Avengers', 'Joss Whedon',
-        'American superhero film based on the Marvel Comics superhero team of the same name'),
--- superhero
+        'American superhero film based on the Marvel Comics superhero team of the same name', 3, 10.00),
        (4, 'How to Train Your Dragon', 'Chris Sanders',
-        'The story takes place in a mythical Viking world where a young Viking teenager named Hiccup aspires to follow his tribe''s tradition of becoming a dragon slayer'),
---                          Cartoon
+        'The story takes place in a mythical Viking world where a young Viking teenager named Hiccup aspires to follow his tribe''s tradition of becoming a dragon slayer',
+        3, 6.67),
        (5, 'Forrest Gump', 'Robert Zemeckis',
-        'The story depicts several decades in the life of Forrest Gump (Hanks), a slow-witted but kind-hearted man from Alabama who witnesses and unwittingly influences several defining historical events in the 20th century United States'),
---                          Drama
+        'The story depicts several decades in the life of Forrest Gump (Hanks), a slow-witted but kind-hearted man from Alabama who witnesses and unwittingly influences several defining historical events in the 20th century United States',
+        4, 7.50),
        (6, 'Avatar', 'James Cameron',
-        'The film is set in the mid-22nd century when humans are colonizing Pandora, a lush habitable moon of a gas giant in the Alpha Centauri star system, in order to mine the valuable mineral unobtanium'),
---                          fantasy
+        'The film is set in the mid-22nd century when humans are colonizing Pandora, a lush habitable moon of a gas giant in the Alpha Centauri star system, in order to mine the valuable mineral unobtanium',
+        2, 1.00),
        (7, 'The Intouchables', 'Olivier Nakache',
-        'The story of the friendship between the two men is then told as a flashback: Philippe, a wealthy quadriplegic who owns a luxurious hôtel particulier, and his assistant Magalie are interviewing candidates to be his live-in caregiver');
--- comedy, drama
+        'The story of the friendship between the two men is then told as a flashback: Philippe, a wealthy quadriplegic who owns a luxurious hôtel particulier, and his assistant Magalie are interviewing candidates to be his live-in caregiver',
+        2, 10.00);
 
-insert into movie_category
+insert into movie_category (movie_id, category_id)
 values (1, 7),
        (1, 11),
        (2, 7),
@@ -54,7 +52,7 @@ values (1, 7),
        (7, 11),
        (7, 4);
 
-insert into feedback
+insert into feedback (id, feedback_text, is_liked, movie_id, user_id)
 values (1, 'some comment', false, 1, 7),
        (2, 'some comment', true, 2, 6),
        (3, 'some comment', true, 3, 5),
@@ -74,12 +72,3 @@ values (1, 'some comment', false, 1, 7),
        (17, 'some comment', true, 4, 4),
        (18, 'some comment', true, 5, 4),
        (19, 'some comment', true, 5, 6);
-
-insert into rate
-values (1, 1.00, 2, 1),
-       (2, 10.00, 3, 2),
-       (3, 10.00, 3, 3),
-       (4, 6.67, 3, 4),
-       (5, 7.50, 4, 5),
-       (6, 1, 2, 6),
-       (7, 10, 2, 7);
