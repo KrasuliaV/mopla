07d7cb371d61d39ed6712795f8e3c515d1c65943
KrasuliaV <Fenix2104>
2021-06-20 22:55:17 +0300

    Added repositories level

--- src/main/java/com/cursor/mopla/repositories/MovieRepository.java
+++ src/main/java/com/cursor/mopla/repositories/MovieRepository.java
@@ -2,8 +2,18 @@ package com.cursor.mopla.repositories;
 
 import com.cursor.mopla.entities.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    List<Movie> getAllByOrderByRateValueAsc();
+
+    List<Movie> getAllByOrderByRateValueDesc();
+
+    @Query(value = "SELECT * FROM movie_category WHERE categoty_id = :categoryId", nativeQuery = true)
+    List<Movie> findAllByCategoryIdAndOrderByName(Long categoryId);
 }
